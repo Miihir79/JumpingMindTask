b2bbcd09a809814c74fc65e30df2eb6cfa36ea65
Miihir79 <mihirrshah02@gmail.com>
2023-04-16 23:12:01 +0530

    toast made better

--- app/src/main/java/com/mihir/jumpingmindtask/ui/Extensionfunctions.kt
+++ app/src/main/java/com/mihir/jumpingmindtask/ui/Extensionfunctions.kt
@@ -4,8 +4,19 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
+private lateinit var toast: Toast
+
+// This file stores all the extension functions to make code concise
+
 fun View.showToast(message:String){
-    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    if (::toast.isInitialized) {
+        toast.cancel()
+    }
+    toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+    toast.show()
 }
 
+/**
+ * Function used to show toast message: [message] Toast.LENGTH_LONG
+ */
 fun Fragment.showToast(message: String) = requireView().showToast(message)
